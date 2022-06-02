cf3444626b389744cc449cafc31ba14e2d66f8e7
aminatd <94138060+aminatd@users.noreply.github.com>
2022-06-02 11:15:37 -0600

    Initial commit

---
+++ src/main/java/com/example/bakerdelivery/BakerdeliveryApplication.java
@@ -0,0 +1,14 @@
+package com.example.bakerdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakerdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakerdeliveryApplication.class, args);
+	}
+
+}
+
